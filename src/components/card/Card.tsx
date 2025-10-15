6bc1fd7b9006903d80aaa2f15583d62cbe14f3c0
Troshina Alesya <a.trowina@gmail.com>
2025-10-15 12:37:59 +0500

    update 'loading data...' card-block

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,47 +1,13 @@
 import { useEffect, useState, type FC } from 'react';
 import type { TCardProps } from './Card.types';
-import { TitleWrapper, Title, Wrapper, LocationIcon, ButtonCity } from './Card.styled';
+import { TitleWrapper, Title, Wrapper, LocationIcon } from './Card.styled';
 import ImageChange from '../../UI/image-change';
 import WeatherInfo from '../../UI/weather-info';
 import CityModal from './components/city-modal';
+import { useWeather } from '../../utils';
 
 const Card: FC<TCardProps> = ({}) => {
-  const [weather, setWeather] = useState({
-    info: { tzinfo: { name: '' } },
-    fact: { wind_speed: 0, humidity: 0, temp: 0 },
-  });
-  const [location, setLocation] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-
-  const getCurrentLocation = () => {
-    if (!location) return;
-
-    //fetch request
-  };
-
-  useEffect(() => {
-    const getLocation = () => {
-      if (!navigator.geolocation) {
-        setShowModal(true);
-        return;
-      }
-
-      navigator.geolocation.getCurrentPosition(
-        async (pos) => {
-          setLocation({ lat: pos.coords.latitude, lon: pos.coords.longitude });
-        },
-        (err) => {
-          setShowModal(true);
-        },
-      );
-    };
-
-    getLocation();
-  }, []);
-
-  useEffect(() => {
-    if (location && !showModal) getCurrentLocation();
-  }, [location, showModal]);
+  const { weather, showModal, setShowModal, setLocation } = useWeather();
 
   if (showModal)
     return (
@@ -56,6 +22,8 @@ const Card: FC<TCardProps> = ({}) => {
         }}
       />
     );
+
+  if (!weather) return <div>Загрузка данных...</div>;
 
   return (
     <Wrapper>
